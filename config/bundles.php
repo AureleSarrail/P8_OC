26f7276a9636e34f4ef025b0c712945801d7b5ad
Aurele Sarrail <aurele.sarrail@gmail.com>
2020-04-15 01:10:10 +0200

    Know adding the first new functionality : Ancient Task now are bond to an Anonymous User. New Tasks are now bond to the authentified User.

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
